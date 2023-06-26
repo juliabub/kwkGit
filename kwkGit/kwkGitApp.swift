dc1f8abb6ab8f33cac95c346d7d49dd50974941d
Julia Bub <21.jtbub@gmail.com>
2023-06-26 09:40:04 -0500

    Initial Commit

---
+++ kwkGit/kwkGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwkGitApp.swift
+//  kwkGit
+//
+//  Created by Julia Bub on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct kwkGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
